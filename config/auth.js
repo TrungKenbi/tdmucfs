efff9ede6ee9982ccae0ca671e335a6824de6797
Phước Trung <trungkenbi@hotmail.com>
2019-06-07 13:53:48 +0700

    Delpy Test

--- config/auth.js
+++ config/auth.js
@@ -4,7 +4,7 @@ module.exports = {
     'facebookAuth' : {
         'clientID'      : '1287536914734391', // your App ID
         'clientSecret'  : '44b88811d922ceb13f464d36d9a3bb94', // your App Secret
-        'callbackURL'   : 'http://localhost:8080/auth/facebook/callback',
+        'callbackURL'   : 'https://tdmucfs.herokuapp.com/auth/facebook/callback',
         'profileFields' : ['id', 'name'] // For requesting permissions from Facebook API
     }
 
