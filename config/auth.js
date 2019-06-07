3727e92fbdc35dcf0af7fd20fa808b30bc684fdc
Phước Trung <trungkenbi@hotmail.com>
2019-06-07 14:02:49 +0700

    Deloy Heroku

--- config/auth.js
+++ config/auth.js
@@ -4,7 +4,9 @@ module.exports = {
     'facebookAuth' : {
         'clientID'      : '1287536914734391', // your App ID
         'clientSecret'  : '44b88811d922ceb13f464d36d9a3bb94', // your App Secret
-        'callbackURL'   : 'https://tdmucfs.herokuapp.com/auth/facebook/callback',
+        'callbackURL'   : (process.env.APP_ENV == 'production' ?
+            'https://tdmucfs.herokuapp.com' :
+            'http://localhost:8080') + '/auth/facebook/callback',
         'profileFields' : ['id', 'name'] // For requesting permissions from Facebook API
     }
 
